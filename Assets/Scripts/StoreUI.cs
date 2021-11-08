69cf6f7fdebf9b312d44f2574b1b7a2c6fd83f96
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-11-08 13:50:50 -0500

    Rod Item Store Tab

--- Assets/Scripts/StoreUI.cs
+++ Assets/Scripts/StoreUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StoreUI : MonoBehaviour
 {
@@ -11,6 +12,13 @@ public class StoreUI : MonoBehaviour
     public Button rodButton;
     public Button lureButton;
     public Button upgradeButton;
+
+    public GameObject listContent;
+    public GameObject itemPrefab;
+
+    public float lakeRodPrice = 50;
+    public float riverRodPrice = 100;
+    public float oceanRodPrice = 250;
 
     // Start is called before the first frame update
     void Start()
@@ -53,6 +61,11 @@ public class StoreUI : MonoBehaviour
             rodButton.enabled = true;
             lureButton.enabled = true;
             upgradeButton.enabled = true;
+
+            foreach (Transform child in listContent.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
         }
         if (tab == 1)
         {
@@ -60,6 +73,59 @@ public class StoreUI : MonoBehaviour
             rodButton.enabled = false;
             lureButton.enabled = true;
             upgradeButton.enabled = true;
+
+            foreach (Transform child in listContent.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+
+            GameObject lakeRod = Instantiate(itemPrefab);
+            lakeRod.transform.SetParent(listContent.transform, false);
+            GameObject lakeRodButton = lakeRod.transform.GetChild(0).gameObject;
+            GameObject lakeRodPriceText = lakeRod.transform.GetChild(1).gameObject;
+            GameObject lakeRodName = lakeRod.transform.GetChild(2).gameObject;
+            if (lakeRodPriceText != null)
+            {
+                TextMeshProUGUI text = lakeRodPriceText.GetComponent<TextMeshProUGUI>();
+                text.text = "$" + lakeRodPrice;
+            }
+            if (lakeRodName != null)
+            {
+                TextMeshProUGUI text = lakeRodName.GetComponent<TextMeshProUGUI>();
+                text.text = "Lake Rod";
+            }
+
+            GameObject riverRod = Instantiate(itemPrefab, new Vector3(0, -100, 0), Quaternion.identity);
+            riverRod.transform.SetParent(listContent.transform, false);
+            GameObject riverRodButton = riverRod.transform.GetChild(0).gameObject;
+            GameObject riverRodPriceText = riverRod.transform.GetChild(1).gameObject;
+            GameObject riverRodName = riverRod.transform.GetChild(2).gameObject;
+            if (riverRodPriceText != null)
+            {
+                TextMeshProUGUI text = riverRodPriceText.GetComponent<TextMeshProUGUI>();
+                text.text = "$" + riverRodPrice;
+            }
+            if (riverRodName != null)
+            {
+                TextMeshProUGUI text = riverRodName.GetComponent<TextMeshProUGUI>();
+                text.text = "River Rod";
+            }
+
+            GameObject oceanRod = Instantiate(itemPrefab, new Vector3(0, -200, 0), Quaternion.identity);
+            oceanRod.transform.SetParent(listContent.transform, false);
+            GameObject oceanRodButton = oceanRod.transform.GetChild(0).gameObject;
+            GameObject oceanRodPriceText = oceanRod.transform.GetChild(1).gameObject;
+            GameObject oceanRodName = oceanRod.transform.GetChild(2).gameObject;
+            if (oceanRodPriceText != null)
+            {
+                TextMeshProUGUI text = oceanRodPriceText.GetComponent<TextMeshProUGUI>();
+                text.text = "$" + oceanRodPrice;
+            }
+            if (oceanRodName != null)
+            {
+                TextMeshProUGUI text = oceanRodName.GetComponent<TextMeshProUGUI>();
+                text.text = "Ocean Rod";
+            }
         }
         if (tab == 2)
         {
@@ -67,6 +133,11 @@ public class StoreUI : MonoBehaviour
             rodButton.enabled = true;
             lureButton.enabled = false;
             upgradeButton.enabled = true;
+
+            foreach (Transform child in listContent.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
         }
         if (tab == 3)
         {
@@ -74,6 +145,11 @@ public class StoreUI : MonoBehaviour
             rodButton.enabled = true;
             lureButton.enabled = true;
             upgradeButton.enabled = false;
+
+            foreach (Transform child in listContent.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
         }
     }
 }
